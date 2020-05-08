0bf2b30d40f74d83fc674bb38de096394634a667
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-08 16:24:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,25 @@
-def create_an_empty_array
-  empty_array =[]
-end
 
-def create_an_array
-  new_array = ["notebook", "pen", "pencil", "eraser"]
-end
 
-def add_element_to_end_of_array(array, element)
-  array << element
-end
+
+
+
+
+
+
+
+
+
+# def create_an_empty_array
+#   empty_array =[]
+# end
+# 
+# def create_an_array
+#   new_array = ["notebook", "pen", "pencil", "eraser"]
+# end
+# 
+# def add_element_to_end_of_array(array, element)
+#   array << element
+# end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
