93275ee4e193d6be7d9a9e65a9798e68a8047431
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-08 16:43:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,11 +1,9 @@
 def create_an_empty_array
   []
-
 end
 
 def create_an_array
   fruits = ["apple","grapes", "mango", "watermelon"]
-
 end
 
 def add_element_to_end_of_array(array, element)
@@ -18,8 +16,19 @@ end
 
 def remove_element_from_end_of_array(array)
   array.pop
-
 end
+
+def remove_element_from_start_of_array(array)
+  array.shift
+end
+
+
+
+
+
+
+
+
 
 
 
